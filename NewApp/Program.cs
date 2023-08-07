ab8708bbec5d68982ad2949a87f16ca734f7897c
Kiennt2512 <ntkienkdtt@gmail.com>
2023-08-07 15:38:20 +0700

    Udate_html

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!")
+
+string hoten = "Nguyen Trung Kien"; //khai báo  biển kiểu string và gán giá trị bằng Nguyễn Trung Kiên
+int tuoi = 35; //khai báo biển kiểu integer và gán giá trị tuổi bằng = 35
+Console.Write("Sinh vien {0} - {1} tuoi", hoten, tuoi);
+
+
+   
+
+
